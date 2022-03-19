6cac79e266ff71597ea15ceaa44be630f371f1b0
Gerson Jair Solano Villegas <USIS051921@ugb.edu.sv>
2022-03-19 15:32:08 -0600

    Agregamos Crud

--- app/src/main/java/com/example/grupo_6_semana_9/MainActivity.java
+++ app/src/main/java/com/example/grupo_6_semana_9/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.grupo_6_semana_9;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.ClipData;
 import android.content.Intent;
@@ -14,28 +16,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.grupo_6_semana_9.adactadores.ListaContactosAdapter;
+import com.example.grupo_6_semana_9.db.DbContactos;
 import com.example.grupo_6_semana_9.db.DbHelper;
+import com.example.grupo_6_semana_9.entidades.Contactos;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnCrear;
+    RecyclerView listaContactos;
+    ArrayList<Contactos> listaArrayContactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnCrear = findViewById(R.id.btnCrear);
-        btnCrear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DbHelper dbHelper = new DbHelper(MainActivity.this);
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                if ( db != null){
-                    Toast.makeText(MainActivity.this , "BASE DE DATOS CREADA",Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(MainActivity.this , "ERROR AL CREAR BASE DE DATOS",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        listaContactos = findViewById(R.id.listaContactos);
+        listaContactos.setLayoutManager(new LinearLayoutManager(this));
+
+        DbContactos dbContactos = new DbContactos(MainActivity.this);
+        listaArrayContactos = new ArrayList<>();
+        ListaContactosAdapter adapter = new ListaContactosAdapter(dbContactos.mostrarContactos());
+        listaContactos.setAdapter(adapter);
+
+
     }
     public  boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
--- app/src/main/java/com/example/grupo_6_semana_9/db/DbContactos.java
+++ app/src/main/java/com/example/grupo_6_semana_9/db/DbContactos.java
@@ -2,9 +2,14 @@ package com.example.grupo_6_semana_9.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import androidx.annotation.Nullable;
+
+import com.example.grupo_6_semana_9.entidades.Contactos;
+
+import java.util.ArrayList;
 
 public class DbContactos extends DbHelper{
 
@@ -28,6 +33,30 @@ public class DbContactos extends DbHelper{
            ex.toString();
         }
         return  id;
+    }
+    public ArrayList <Contactos> mostrarContactos (){
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ArrayList<Contactos> listaContactos = new ArrayList<>();
+        Contactos contacto = null;
+        Cursor cursorContactos = null;
+        cursorContactos = db.rawQuery("SELECT * FROM " + TABLE_CONTACTOS, null);
+        if (cursorContactos.moveToFirst()){
+            do {
+                contacto = new Contactos();
+                contacto.setId(cursorContactos.getInt(0));
+                contacto.setNombre(cursorContactos.getString(1));
+                contacto.setTelefono(cursorContactos.getString(2));
+                contacto.setCorreo_electronico(cursorContactos.getString(3));
+                listaContactos.add(contacto);
+
+            } while(cursorContactos.moveToNext());
+
+
+        }
+        cursorContactos.close();
+        return listaContactos;
+
     }
 
 }
