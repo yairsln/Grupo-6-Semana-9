f40c8e63c97a42dac4e304bd5facc1a9bfce2422
Gerson Jair Solano Villegas <USIS051921@ugb.edu.sv>
2022-03-19 09:05:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/grupo_6_semana_9/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.grupo_6_semana_9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.grupo_6_semana_9.db.DbHelper;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnCrear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCrear = findViewById(R.id.btnCrear);
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                if ( db != null){
+                    Toast.makeText(MainActivity.this , "BASE DE DATOS CREADA",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this , "ERROR AL CREAR BASE DE DATOS",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public  boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return  true;
+
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menuNuevo:
+                nuevoRegistro();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+    private void  nuevoRegistro(){
+        Intent intent = new Intent(this,nuevoActivity.class);
+        startActivity(intent);
+    }
+}
